ea6ca0a692056fad19bb5e9dd44775de510b24af
unknown <arpitpatidar1999@gmail.com>
2022-06-09 01:28:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from "axios";
 import './App.css';
+import Pokemon from './Pokemon';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://pokeapi.co/api/v2/pokemon/"
+      )
+      .then((res) => {
+        setPokemon(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+  const filteredPokemon = pokemon.filter((pokemons) =>
+    pokemons.name.toLowerCase().includes(search.toLowerCase())
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="pokemon-app">
+      <div className="pokemon-search">
+        <form action="">
+          <input
+            type="text"
+            className="pokemon-input"
+            placeholder="Search Pokemon"
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      {filteredPokemon.map((pokemon) => {
+        return (
+          <Pokemon
+            name={pokemon.name}
+          />
+        );
+      })}
+      </div>
     </div>
   );
 }
